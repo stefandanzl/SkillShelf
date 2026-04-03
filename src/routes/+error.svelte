945600d91f85aecbbe1ca94f2b75d37c346a176a
Stefan Danzl <stefan.danzl@live.de>
2026-04-03 03:21:23 +0200

    origin env

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-  import { page } from "$app/state";
+	import { page } from '$app/state';
 
-  console.log(page)
+	console.log(JSON.stringify(page, null, 2));
 </script>
 
 <h1>{page.status}</h1>
 <p>{page.error?.message}</p>
 
 <style>
-  p {
-    margin-inline: auto;
-    text-align: center;
-  }
+	p {
+		margin-inline: auto;
+		text-align: center;
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -15,13 +15,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: ''
-		},
-		csrf: {
-			checkOrigin: false,
-			trustedOrigins: ['https://skillshelf.danzl.it']
-		},
 		alias: {
 			$utils: 'src/utils',
 			$routes: 'src/routes'
